3cfb090c87fb9e1b2bcdc269806b770d23ca4f0e
abhayvishnoi <abhayvishnoi6@gmail.com>
2022-02-05 01:03:53 +0530

    added support for mobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,18 @@
 import React from "react";
 import reactLogo from "../images/logo512.png";
+import { FaSun } from "react-icons/fa";
+import { RiMoonClearFill } from "react-icons/ri";
 export default function Navbar(props) {
   return (
     <nav className={props.darkMode ? "navbar dark" : "navbar"}>
-      <img src={reactLogo} alt="Logo" className="nav-icon" />
+      <img src={reactLogo} alt="Logo" className="nav-logo" />
       <h3 className="nav_logo_text">ReactFacts</h3>
-      <h4 className="nav-toggle">
-        <span id={props.darkMode ? "light-text" : ""}>Light</span>
-        <label className="switch">
-          <input type="checkbox" onClick={props.toggleDarkMode} />
-          <span className="slider round"></span>
-        </label>
-        <span id={props.darkMode ? "dark-text" : "lessopac"}>Dark</span>
+      <h4 className="nav-toggle" onClick={props.toggleDarkMode}>
+        {props.darkMode ? (
+          <FaSun className="nav-icon light-icon" />
+        ) : (
+          <RiMoonClearFill className="nav-icon" />
+        )}
       </h4>
     </nav>
   );
